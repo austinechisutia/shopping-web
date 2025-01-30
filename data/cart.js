94ea5f94736ee4598c107c7e3d023d9e2b89e061
austinechisutia <austineakhonya624@gmail.com>
2025-01-30 12:10:39 +0300

    save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,21 @@
-export let cart = [{
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 1
-}, {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1
-}];
+export let cart = JSON.parse(localStorage.getItem("cart")) 
+
+if(!cart){ 
+    cart = [{
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 1
+  }, {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 1
+  }];;
+}
+
+
+
+
+function saveToStorage(){
+    localStorage.setItem("cart", JSON.stringify(cart));
+  } // save the cart to local storage
 
 export function addToCart(productId){
     let martchingItem;
@@ -23,6 +34,7 @@ export function addToCart(productId){
           });
         }
   
+        saveToStorage();
          // add the product to the cart
   }
 
@@ -37,4 +49,6 @@ export function addToCart(productId){
     });
 
     cart = newCart;
+
+    saveToStorage();
   }
